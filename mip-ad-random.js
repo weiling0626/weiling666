bae00c419a81df9c98b85d6ce73dd90c20cc7aeb
weiling0626 <741999251@qq.com>
2016-11-30 14:27:59 +0800

    Update mip-ad-random.js

--- mip-ad-random.js
+++ mip-ad-random.js
@@ -4,20 +4,20 @@ define("mip-ad-random", ["require", "customElement", "zepto"], function (i) {
             if (!t.isRender) {
                 t.isRender = !0;
 
-                function getadress() {
+                function GetPosition() {
                     
                     //随机要放入广告的大位置
-                    var allLocation = $("mip-ad-random").data("randomad"); 
+                    var allLocation = $("mip-ad-random").data("parents"); 
                     var bigLocation = $('.' + allLocation);
-                    var arry_length1 = arry1.length;
-                    var rand_num = Math.ceil(Math.random() * arry_length1) - 1;
-                    var rank = bigLocation[rand_num];
+                    var arrylength1 = bigLocation.length;
+                    var randnum = Math.ceil(Math.random() * arrylength1) - 1;
+                    var rank = bigLocation[randnum];
 
                     //随机子元素放入广告
-                    var r_children = $(rank).children();
-                    var arry_length2 = r_children.length;
-                    var rand_num2 = Math.ceil(Math.random() * arry_length2) - 1;
-                    var rank2 = r_children[rand_num2];
+                    var rchildren = $(rank).children();
+                    var arrylength2 = rchildren.length;
+                    var randnum2 = Math.ceil(Math.random() * arrylength2) - 1;
+                    var rank2 = rchildren[randnum2];
                     var adSrc = t.getAttribute('ad-src'); //广告图片路径
                     var adHref = t.getAttribute('ad-href'); //广告链接路径
                     var adTitle = t.getAttribute('ad-title'); //广告标题
@@ -27,13 +27,13 @@ define("mip-ad-random", ["require", "customElement", "zepto"], function (i) {
                     
                     //判断：若大位置没有子元素，就在其里面添加广告，若大位置有子元素，在随机子元素的后面添加广告
                     var advet = '<mip-ad layout="reponsive" width="' + adWidth + '" height="' + adHeight + '"  type="ad-comm"tpl="oneImg" class="' + adClass + '" href="' + adHref + '" data-size="1242 180" src="' + adSrc + '" data-title="' + adTitle + '"></mip-ad>';
-                    if (arry_length2 == 0) {
+                    if (arrylength2 == 0) {
                         $(rank).append(advet);
                     } else {
                         $(rank2).after(advet);
                     }
                 }
-                getadress();
+                GetPosition();
             }
         }
         var $ = i("zepto");
